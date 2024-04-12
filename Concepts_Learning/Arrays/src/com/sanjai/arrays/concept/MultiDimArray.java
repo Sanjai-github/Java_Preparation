4c7f5b42620dd8ac9fd52ea926680561ef9a8e99
Sanjai-github <sanjai.social2002@gmail.com>
2024-04-13 00:59:25 +0530

    Practised Array questions day2 :)

--- Concepts_Learning/Arrays/src/com/sanjai/arrays/concept/MultiDimArray.java
+++ Concepts_Learning/Arrays/src/com/sanjai/arrays/concept/MultiDimArray.java
@@ -1,5 +1,7 @@
 package com.sanjai.arrays.concept;
 
+import java.util.Arrays;
+
 public class MultiDimArray {
     /* in the two dimension array the row of the main array itself is an array.
     * the two-dimensional arrays are in the form of matrices
@@ -14,11 +16,11 @@ public class MultiDimArray {
                 {8, 9,}
         };
        // print the two Dimensional Array with the for-each loop
-//        for (int[] a:arr) {
-//            System.out.println(Arrays.toString(a));
-//            for (int b:a)
-//                System.out.println(b);
-//        }
+        for (int[] a:arr) {
+            System.out.println(Arrays.toString(a));
+            for (int b:a)
+                System.out.println(b);
+        }
         // print the two Dimensional Array with the for loop
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
