8aa1a714bf6f3fbd9c193da52f463d748745b2bf
Sanjai-github <sanjai.social2002@gmail.com>
2024-04-14 19:07:04 +0530

    Practised Array questions day2 :)

--- Concepts_Learning/Arrays/src/com/sanjai/arrays/questions/FirstAndLastPositionOfElement.java
+++ Concepts_Learning/Arrays/src/com/sanjai/arrays/questions/FirstAndLastPositionOfElement.java
@@ -9,21 +9,24 @@ Output: [3,4]
 public class FirstAndLastPositionOfElement {
     public static void main(String[] args) {
         int []nums = {5,7,7,8,8,10} ;
-        int target = 7;
+        int target = 8;
         int []ans = searchRange(nums,target);
         System.out.println(Arrays.toString(ans));
     }
     static int[] searchRange(int[] nums, int target) {
-        int start = 0,end=0;
-        int[] ans = {start,end};
+        int[] ans = {-1,-1};
+        if(nums.length == 1){
+            ans[0] = 0;
+            ans[1] = 0;
+        }
         for(int i = 0; i<nums.length - 1; i++){
             if (nums[i] == target){
-                 start = i;
+                ans[1] = i;
             }
         }
         for (int j = nums.length - 1; j > 0; j--){
             if (nums[j] == target){
-                end = j;
+                ans[0] = j;
             }
         }
         return ans;
