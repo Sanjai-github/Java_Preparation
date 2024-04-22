652ed6526a825c21321e800fc110bd22d11498c2
Sanjai-github <sanjai.social2002@gmail.com>
2024-04-22 20:42:41 +0530

    Added Practiced Questions

--- Concepts_Learning/Arrays/src/com/sanjai/arrays/questions/FirstAndLastPositionOfElement.java
+++ Concepts_Learning/Arrays/src/com/sanjai/arrays/questions/FirstAndLastPositionOfElement.java
@@ -8,26 +8,40 @@ Output: [3,4]
 */
 public class FirstAndLastPositionOfElement {
     public static void main(String[] args) {
-        int []nums = {5,7,7,8,8,10} ;
-        int target = 8;
+        int []nums = {0,0,1,2,2} ;
+        int target = 2;
         int []ans = searchRange(nums,target);
         System.out.println(Arrays.toString(ans));
     }
     static int[] searchRange(int[] nums, int target) {
-        int[] ans = {-1,-1};
-        if(nums.length == 1){
-            ans[0] = 0;
-            ans[1] = 0;
-        }
-        for(int i = 0; i<nums.length - 1; i++){
-            if (nums[i] == target){
-                ans[1] = i;
+        int[] ans = {-1, -1};
+        int start = search(nums,target,true);
+        int end = search(nums,target,false);
+        ans[0] = start;
+        ans[1] = end;
+        return ans;
+    }
+    static int search(int []nums,int target,boolean isFirst){
+        int ans =-1;
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end){
+            int mid = start+(end - start)/2;
+            if(target == nums[mid]){
+                ans = mid;
+                if(isFirst){
+                    end = mid - 1;
+                }
+                else{
+                    start = mid + 1;
+                }
+            }else if (target > nums[mid]) {
+                start = mid + 1;
             }
-        }
-        for (int j = nums.length - 1; j > 0; j--){
-            if (nums[j] == target){
-                ans[0] = j;
+            else{
+                end = mid - 1;
             }
+
         }
         return ans;
     }
--- Concepts_Learning/Arrays/src/com/sanjai/arrays/sorting/Bubble.java
+++ Concepts_Learning/Arrays/src/com/sanjai/arrays/sorting/Bubble.java
@@ -1,7 +1,34 @@
 package com.sanjai.arrays.sorting;
 
+import java.util.Arrays;
+
 public class Bubble {
     public static void main(String[] args) {
+        int []arr = {3, 5, 1, 2, 4};
+        search(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void search(int []arr){
+        boolean swaped ;
+        for (int i = 0; i < arr.length; i++) {
+            swaped = false;
+            for (int j = 1; j < arr.length-i ; j++) {
+                // if the element in the j-index is less than the element int the (j-1) - index then swap these two elements
+                if (arr[j] < arr[j-1]){
+                    swap(arr, j,j-1);
+                    swaped = true;
+                }
+                /*
+                * if the swap doesn't take place for an entire pass that means that array is sorted then the loop will break */
+            }if (!swaped){
+                break;
+            }
+        }
+    }
 
+    static void swap(int[] arr, int first, int last) {
+        int temp = arr[first];
+        arr[first] = arr[last];
+        arr[last] = temp;
     }
 }
